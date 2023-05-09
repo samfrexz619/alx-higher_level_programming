5932df7247e9a134f759f74c74cf7696804e9326
samuel Damilola <samfrexz69@gmail.com>
2023-05-09 10:47:23 -0700

    add file

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for idx in range(1, 101):
+        if idx % 3 == 0 and idx % 5 == 0:
+            print('FizzBuzz', end='')
+        elif idx % 3 == 0:
+            print('Fizz', end='')
+        elif idx % 5 == 0:
+            print('Buzz', end='')
+        else:
+            print(idx, end='')
+
+        print(' ', end='')
