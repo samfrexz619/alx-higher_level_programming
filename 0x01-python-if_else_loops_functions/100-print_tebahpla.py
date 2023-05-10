8ed6313dfb849b19e79ef2227339dacac3f652c8
samuel Damilola <samfrexz69@gmail.com>
2023-05-10 01:20:12 -0700

    refactored 100-print_tebahpla file

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for alph in reversed(range(97, 123)):
-    print('{:c}'.format(alph if (alph % 2 == 0) else (alph - 32)), end='')
+    print(f'{alph if (alph % 2 == 0) else (alph - 32):c}', end='')
