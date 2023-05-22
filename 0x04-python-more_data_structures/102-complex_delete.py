a09c4856b3ca05e27b2efa65e29eef307ea836cf
samuel Damilola <samfrexz69@gmail.com>
2023-05-22 00:03:29 -0700

    add files

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    list_k = list(a_dictionary.keys())
+
+    for vd in list_k:
+        if value == a_dictionary.get(vd):
+            del a_dictionary[vd]
+
+        return (a_dictionary)
