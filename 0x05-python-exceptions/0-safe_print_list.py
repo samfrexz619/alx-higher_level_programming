f948c246de5ff5ad7503da72ba718335ccd0327b
samuel Damilola <samfrexz69@gmail.com>
2023-05-29 04:55:08 -0700

    add dir

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    sliced_list = my_list[:x]
+    count = 0
+
+    for idx in sliced_list:
+        try:
+            print(f'{idx}', end='')
+        except:
+            break
+        else:
+            count += 1
+
+    print()
+    return (count)
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+This project is on exception handling in python
