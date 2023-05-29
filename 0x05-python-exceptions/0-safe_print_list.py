7ff1778cd56a157f85c4dca110511c942a5bd098
samuel Damilola <samfrexz69@gmail.com>
2023-05-29 05:09:15 -0700

    upd file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
     for idx in sliced_list:
         try:
             print('{}'.format(idx), end='')
-        except:
+        except Exception:
             break
         else:
             count += 1
