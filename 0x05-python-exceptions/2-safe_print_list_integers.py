e2ea90fa01a44a0510028ca6429274011b8727b9
samuel Damilola <samfrexz69@gmail.com>
2023-05-29 06:09:42 -0700

    upd file

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
         except TypeError:
             pass
         except ValueError:
-            continue
+            pass
         else:
             count += 1
 
