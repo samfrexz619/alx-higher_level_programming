570439b986d7eecf4406d637332ca01e437fabd2
samuel Damilola <samfrexz69@gmail.com>
2023-05-29 05:32:13 -0700

    upd file

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
+    sliced_list = my_list[:x]
 
-    for idx in my_list[:x]:
+    for idx in sliced_list:
         try:
             print(f'{idx:d}', end='')
         except TypeError:
             pass
         except ValueError:
-            continue
+            pass
         else:
             count += 1
 
