49a55cbda0a39df13fc59f5852f28fb94e62b52b
samuel Damilola <samfrexz69@gmail.com>
2023-05-29 12:46:45 -0700

    add files

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+    
+    try:
+        res = fct(*args)
+    except Exception as idx:
+        sys.stderr.write("Exception: {}\n".format(idx))
+        res = None
+    return (result)
