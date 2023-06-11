c592f34ae0bd174f30ec50dfa854043672896c30
samuel Damilola <samfrexz69@gmail.com>
2023-06-11 08:14:19 -0700

    add dir

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def lookup(obj):
+    '''function that returns the list of available attr
+
+    Args:
+        obj: instance of class
+
+    Returns: 
+        list of attrs
+    '''
+
+    return dir(obj)
