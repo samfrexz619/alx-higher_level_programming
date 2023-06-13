143569f466cec8665265d7367debe6faebe1c99a
samuel Damilola <samfrexz69@gmail.com>
2023-06-13 07:37:42 -0700

    add file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''module that writes to a txt file'''
+
+
+def append_write(filename="", text=""):
+    '''func that rets char count
+
+    Args:
+        filename: filename
+        text: text to write
+
+    Raises:
+        Exception: when file can be opened
+
+    '''
+
+    count_char = 0
+
+    with open(filename, mode='a', encoding='utf-8') as f:
+        count_char = f.write(text)
+
+    return count_char
--- 0x0B-python-input_output/main.py
+++ 0x0B-python-input_output/main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-write_file = __import__('1-write_file').write_file
+write_file = __import__('2-append_write').append_write
 
 nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
 print(nb_characters)
