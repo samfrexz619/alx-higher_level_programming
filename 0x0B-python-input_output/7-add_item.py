d18b5a4c76234a01d500ac1e8f866c837e2e6ffe
samuel Damilola <samfrexz69@gmail.com>
2023-06-13 10:24:25 -0700

    upd file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,8 +5,6 @@
 import sys
 import os
 
-'''import files from dir'''
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
