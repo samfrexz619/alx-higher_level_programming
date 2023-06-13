74a9fd1cc02feaef403bc649ad380ee6aec94bf2
samuel Damilola <samfrexz69@gmail.com>
2023-06-13 08:31:21 -0700

    upd file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,8 +7,10 @@ import os
 
 '''import files from dir'''
 
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+'''import file from dir'''
+
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if os.path.exists('add_item.json') is True:
