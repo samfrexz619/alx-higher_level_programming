b9d370a97963f0aab5f1e1f723a65a98034a254e
samuel Damilola <samfrexz69@gmail.com>
2023-06-17 11:51:44 -0700

    upd file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -44,3 +44,73 @@ class Rectangle(Base):
         if val <= 0:
             raise ValueError('height must be > 0')
         self.__height = value
+
+    @property
+    def x(self):
+        '''x getter'''
+        return self.__x
+
+    @x.setter
+    def x(self, val):
+        '''x setter'''
+        if type(val) is not int:
+            raise TypeError('x must be an integer')
+        if val < 0:
+            raise ValueError('x must be >= 0')
+        self.__x = val
+
+    @property
+    def y(self):
+        '''y getter'''
+        return self.__y
+
+    @y.setter
+    def y(self, val):
+        '''y setter'''
+        if type(val) is not int:
+            raise TypeError('y must be an integer')
+        if val < 0:
+            raise ValueError('y must be >= 0')
+        self.__y = val
+
+    def area(self):
+        '''rets area of the rectangle'''
+        return self.width * self.height
+
+    def display(self):
+        '''displays a rect'''
+        rect = self.y * '\n'
+        for idx in range(self.height):
+            rect += ('' * self.x)
+            rect += ('#' * self.width) + '\n'
+
+        print(rect, end='')
+
+    def __str__(self):
+        '''str method'''
+        str_rect = '[Rectangle]'
+        str_id = f'({self.id}) '
+        str_xy = f'{self.x}/{self.y} - '
+        str_wh = f'{self.width}/{self.height}'
+
+        return str_rect + str_id + str_xy + str_wh
+
+    def update(self, *args, **kwargs):
+        '''update method'''
+        if args is not None and len(args) is not 0:
+            ls_atr = ['id', 'width', 'height', 'x', 'y']
+            for idx in range(len(args)):
+                setattr(self, ls_str[idx], args[idx])
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dict(self):
+        '''method to ret a dict with properties'''
+        ls_atr = ['id', 'width', 'height', 'x', 'y']
+        d_res = {}
+
+        for key in ls_atr:
+            d_res[key] = getattr(self, key)
+
+        return d_res
