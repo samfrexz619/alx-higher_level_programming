00aff20eca18677316ceae66ec10d6f8f629b224
samuel Damilola <samfrexz69@gmail.com>
2023-06-19 14:13:10 -0700

    upd file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''module that contains the base class'''
+'''Module that contains class Base'''
 
 import json
 import csv
@@ -9,13 +9,11 @@ import os.path
 
 
 class Base:
-    '''class Base'''
-
+    """ Class Base """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        '''init instance'''
-
+        '''init instances'''
         if id is not None:
             self.id = id
         else:
@@ -24,15 +22,15 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        '''method that ret json str'''
-        if list_dictionaries is None or list_dictionaries == '[]':
-            return '[]'
+        '''Ls to JSON str'''
+        if list_dictionaries is None or list_dictionaries == "[]":
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
-    def save_to_file_csv(cls, list_objs):
-        '''save obj in a file'''
-        f_name = f'{cls.__name__}.json'
+    def save_to_file(cls, list_objs):
+        '''Save obj in a file'''
+        f_name = "{}.json".format(cls.__name__)
         ls_dic = []
 
         if not list_objs:
@@ -55,7 +53,7 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
-        '''create an instance'''
+        '''Create an instance'''
         if cls.__name__ == 'Rectangle':
             nw = cls(10, 10)
         else:
@@ -64,8 +62,8 @@ class Base:
         return nw
 
     @classmethod
-    def load_from_file_csv(cls):
-        '''ret a list of instance'''
+    def load_from_file(cls):
+        '''Returns a list of instances'''
         f_name = f'{cls.__name__}.json'
 
         if os.path.exists(f_name) is False:
@@ -84,27 +82,58 @@ class Base:
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
-        '''method to save a csv file'''
+        '''method that saves a CSV file'''
         f_name = f'{cls.__name__}.csv'
 
-        if cls.__name__ == 'Rectangle':
-            ls_dict = [0, 0, 0, 0, 0]
+        if cls.__name__ == "Rectangle":
+            ls_dic = [0, 0, 0, 0, 0]
             ls_keys = ['id', 'width', 'height', 'x', 'y']
         else:
-            ls_dict = ['0', '0', '0', '0']
+            ls_dic = ['0', '0', '0', '0']
             ls_keys = ['id', 'size', 'x', 'y']
 
         matx = []
 
         if not list_objs:
             pass
-
         else:
-            for ob in list_objs:
+            for obj in list_objs:
                 for ky in range(len(ls_keys)):
-                    ls_dict[ky] = ob.to_dictionary()[ls_keys[ky]]
-                matx.append(ls_dict[:])
+                    ls_dic[ky] = obj.to_dictionary()[ls_keys[ky]]
+                matx.append(ls_dic[:])
 
         with open(f_name, 'w') as wFile:
             writer = csv.writer(wFile)
             writer.writerows(matx)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        '''method that loads a CSV file'''
+        f_name = f'{cls.__name__}.csv'
+
+        if os.path.exists(f_name) is False:
+            return []
+
+        with open(f_name, 'r') as rFile:
+            rdr = csv.reader(rFile)
+            csv_ls = list(rdr)
+
+        if cls.__name__ == 'Rectangle':
+            ls_keys = ['id', 'width', 'height', 'x', 'y']
+        else:
+            ls_keys = ['id', 'size', 'x', 'y']
+
+        matx = []
+
+        for elem in csv_ls:
+            d_csv = {}
+            for kv in enumerate(elem):
+                d_csv[ls_keys[kv[0]]] = int(kv[1])
+            matx.append(d_csv)
+
+        ls_ins = []
+
+        for idx in range(len(matx)):
+            ls_ins.append(cls.create(**matx[idx]))
+
+        return ls_ins
