6c7aace795c66cc6c18536e76b369f685c3a589f
samuel Damilola <samfrexz69@gmail.com>
2023-08-14 09:19:55 -0700

    add files

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 function add (a, b) {
-	const sum = a + b;
-	return console.log(sum);
+  return a + b;
 }
-add(Number(process.argv[2]), Number(process.argv[3]));
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
