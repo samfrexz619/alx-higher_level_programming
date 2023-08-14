453b81e7531c695bb61e842fc76731e9ff2b16fa
samuel Damilola <samfrexz69@gmail.com>
2023-08-14 05:00:00 -0700

    upd task 2 and 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-process.argv[2] === undefined ?
-	console.log('No argument') :
-	console.log(process.argv[2]);
+process.argv[2] === undefined
+	? console.log('No argument')
+	: console.log(process.argv[2]);
