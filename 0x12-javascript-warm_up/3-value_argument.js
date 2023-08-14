c053553a7ffebc9b9091b90019665fa045223266
samuel Damilola <samfrexz69@gmail.com>
2023-08-14 07:04:51 -0700

    upd files

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-process.argv[2] === undefined
-	? console.log('No argument')
-	: console.log(process.argv[2])
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,4 +2,4 @@
 const sentences = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < sentences.length; i++) {
 	console.log(sentences[i]);
-};
+}
