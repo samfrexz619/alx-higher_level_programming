7eb384d6354ea03fe46acda7c705b6f2bf2c288f
samuel Damilola <samfrexz69@gmail.com>
2023-08-15 09:34:15 -0700

    upd file

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-const f1 = fs.readFileSync(process.argv[2]);
-const f2 = fs.readFileSync(process.argv[3]);
-fs.writeFileSync(process.argv[4], f1 + f2, 'utf-8');
+
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
