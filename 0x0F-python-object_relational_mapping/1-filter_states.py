45c6986ea9ebd5a841831ecfde67e87db2db78d7
samuel Damilola <samfrexz69@gmail.com>
2023-08-19 01:35:14 -0700

    add files

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,12 +10,5 @@ if __name__ == "__main__":
                          db=sys.argv[3], port=3306)
 
     cr = db.cursor()
-    cr.execute("SELECT * \
-    FROM states \
-    WHERE CONVERT(`name` USING Latin1) \
-    COLLATE Latin1_General_CS \
-    LIKE 'N%';")
-    states = cr.fetchall()
-
-    for state in states:
-        print(state)
+    cr.execute("SELECT * FROM states ORDER BY id")
+    [print(state) for state in cr.fetchall() if state[1][0] == "N"]
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,11 +10,6 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
     cr = db.cursor()
-    cr.execute("SELECT * \
-    FROM states \
-    WHERE CONVERT(`name` USING Latin1) \
-    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
-    states = cr.fetchall()
-
-    for state in states:
-        print(state)
+    cr.execute("SELECT * FROM states WHERE BINARY name = '{}'"
+               .format(sys.argv[4]))
+    [print(state) for state in cr.fetchall()]
