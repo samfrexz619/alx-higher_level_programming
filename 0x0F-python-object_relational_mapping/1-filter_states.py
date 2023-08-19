d5626c488042c7c7cb6f8cba6738dde70e8514c9
samuel Damilola <samfrexz69@gmail.com>
2023-08-19 01:12:04 -0700

    update files

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,12 +2,18 @@
 """
 Lists all states with a name starting with N
 """
-
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cr = db.cursor()
-    cr.execute("SELECT * FROM states ORDER BY id")
-    [print(state) for state in cr.fetchall() if state[1][0] == "N"]
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS \
+    LIKE 'N%';")
+    states = cr.fetchall()
+
+    for state in states:
+        print(state)
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,13 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cr = db.cursor()
-    cr.execute("SELECT * FROM states WHERE BINARY name = f'{sys.argv[4]}'")
-    [print(state) for state in cr.fetchall()]
+    cr.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
+    states = cr.fetchall()
+
+    for state in states:
+        print(state)
