f854fd76fcfef1d8c14e152d1a79265f450bf8ea
samuel Damilola <samfrexz69@gmail.com>
2023-08-19 02:45:00 -0700

    add files

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 '''
-delete state obj starting with a
+print city obj
 '''
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import State
+from model_city import City
 
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
@@ -15,9 +16,7 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).filter(State.name.like('%a%')).all()
-
-    for state in states:
-        session.delete(state)
-
-    session.commit()
+    for city, state in session.query(City, State) \
+                              .filter(City.state_id == State.id) \
+                              .order_by(City.id):
+        print(f'{state.name}: {(city.id)} {city.name}')
